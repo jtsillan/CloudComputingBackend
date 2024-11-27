4b5de1a5868ee45183d9870406112aa1c245f7fb
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2024-11-27 18:12:59 +0200

    change app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Docker World!</p>"
+    return "<p>Hello, Docker World and Dogs!</p>"
 
 
 if __name__ == "__main__":
